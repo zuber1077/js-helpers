ae42c0d28bb63b26c5f2c0d9b1ff559a70b56386
zuber1077 <zuber1077@yahoo.com>
2019-04-08 10:22:30 +0800

    fix bugs and lint code

--- utils.js
+++ utils.js
@@ -1,9 +1,8 @@
-// Function throttling
-/* eslint-disable */
+// import { isIOS, addClass, removeClass } from './validators'
 export const debounce = (func, delay = 200) => {
   let timer = null
 
-  return function (...args) {
+  return function(...args) {
     timer && clearTimeout(timer)
     timer = setTimeout(() => {
       func.apply(this, args)
@@ -11,27 +10,33 @@ export const debounce = (func, delay = 200) => {
   }
 }
 
-export function formatDate (date, fmt) {
+export function formatDate(date, fmt) {
   if (/(y+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+    fmt = fmt.replace(
+      RegExp.$1,
+      (date.getFullYear() + '').substr(4 - RegExp.$1.length)
+    )
   }
-  let o = {
+  const o = {
     'M+': date.getMonth() + 1,
     'd+': date.getDate(),
     'h+': date.getHours(),
     'm+': date.getMinutes(),
     's+': date.getSeconds()
   }
-  for (let k in o) {
+  for (const k in o) {
     if (new RegExp(`(${k})`).test(fmt)) {
-      let str = o[k] + ''
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str))
+      const str = o[k] + ''
+      fmt = fmt.replace(
+        RegExp.$1,
+        RegExp.$1.length === 1 ? str : padLeftZero(str)
+      )
     }
   }
   return fmt
-};
+}
 
-function padLeftZero (str) {
+function padLeftZero(str) {
   return ('00' + str).substr(str.length)
 }
 
@@ -39,22 +44,22 @@ function padLeftZero (str) {
  * @example ?id=1234&a=b
  * @return Object {id: 12345, a: b}
  */
-export function urlParse () {
-  let url = window.location.search
-  let obj = {}
-  let reg = /[?&][^?&]+=[^?&]+/g
-  let arr = url.match(reg)
+export function urlParse() {
+  const url = window.location.search
+  const obj = {}
+  const reg = /[?&][^?&]+=[^?&]+/g
+  const arr = url.match(reg)
   // ['?id=12345', '&a=b']
   if (arr) {
-    arr.forEach((item) => {
-      let tempArr = item.substring(1).split('=')
-      let key = decodeURIComponent(tempArr[0])
-      let val = decodeURIComponent(tempArr[1])
+    arr.forEach(item => {
+      const tempArr = item.substring(1).split('=')
+      const key = decodeURIComponent(tempArr[0])
+      const val = decodeURIComponent(tempArr[1])
       obj[key] = val
     })
   }
   return obj
-};
+}
 
 export function realPx(px) {
   const maxWidth = window.innerWidth > 500 ? 500 : window.innerWidth
@@ -66,92 +71,100 @@ export function px2rem(px) {
   return px / ratio
 }
 
-export const pageScroll = (function () {
-  const fn = function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-  };
-  let islock = false;
-
-  return {
-      lock(el) {
-          if (islock) return;
-          islock = true;
-          (el || document).addEventListener('touchmove', fn);
-      },
-      unlock(el) {
-          islock = false;
-          (el || document).removeEventListener('touchmove', fn);
-      }
-  };
-})();
-
-export const preventScroll = (function () {
-  return {
-      lock(el) {
-          isIOS && addClass(el || document.body, 'g-fix-ios-prevent-scroll');
-      },
-      unlock(el) {
-          isIOS && removeClass(el || document.body, 'g-fix-ios-prevent-scroll');
-      }
-  };
-})();
-
-export const getScrollview = function (el) {
-  let currentNode = el;
-  while (currentNode && currentNode.tagName !== 'HTML' && currentNode.tagName !== 'BODY' && currentNode.nodeType === 1) {
-      let overflowY = document.defaultView.getComputedStyle(currentNode).overflowY;
-      if (overflowY === 'scroll' || overflowY === 'auto') {
-          return currentNode;
-      }
-      currentNode = currentNode.parentNode;
+export const pageScroll = (function() {
+  const fn = function(e) {
+    e.preventDefault()
+    e.stopPropagation()
   }
-  return window;
-};
+  let islock = false
 
+  return {
+    lock(el) {
+      if (islock) return
+      islock = true
+      ;(el || document).addEventListener('touchmove', fn)
+    },
+    unlock(el) {
+      islock = false
+      ;(el || document).removeEventListener('touchmove', fn)
+    }
+  }
+})()
 
-export const checkInview = function (scrollView, el) {
-  const contentHeight = scrollView === window ? document.body.offsetHeight : scrollView.offsetHeight;
-  const contentTop = scrollView === window ? 0 : scrollView.getBoundingClientRect().top;
+export const preventScroll = (function() {
+  return {
+    lock(el) {
+      isIOS && addClass(el || document.body, 'g-fix-ios-prevent-scroll')
+    },
+    unlock(el) {
+      isIOS && removeClass(el || document.body, 'g-fix-ios-prevent-scroll')
+    }
+  }
+})()
 
-  const post = el.getBoundingClientRect().top - contentTop;
-  const posb = post + el.offsetHeight;
+export const getScrollview = function(el) {
+  let currentNode = el
+  while (
+    currentNode &&
+    currentNode.tagName !== 'HTML' &&
+    currentNode.tagName !== 'BODY' &&
+    currentNode.nodeType === 1
+  ) {
+    const overflowY = document.defaultView.getComputedStyle(currentNode)
+      .overflowY
+    if (overflowY === 'scroll' || overflowY === 'auto') {
+      return currentNode
+    }
+    currentNode = currentNode.parentNode
+  }
+  return window
+}
 
-  return (post >= 0 && post < contentHeight) || (posb > 0 && posb <= contentHeight);
-};
+export const checkInview = function(scrollView, el) {
+  const contentHeight =
+    scrollView === window ? document.body.offsetHeight : scrollView.offsetHeight
+  const contentTop =
+    scrollView === window ? 0 : scrollView.getBoundingClientRect().top
 
-export const scrollTop = function (el, from = 0, to, duration = 500, callback) {
+  const post = el.getBoundingClientRect().top - contentTop
+  const posb = post + el.offsetHeight
+
+  return (
+    (post >= 0 && post < contentHeight) || (posb > 0 && posb <= contentHeight)
+  )
+}
+
+export const scrollTop = function(el, from = 0, to, duration = 500, callback) {
   if (!window.requestAnimationFrame) {
-      window.requestAnimationFrame = (
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame ||
-          window.msRequestAnimationFrame ||
-          function (callback) {
-              return window.setTimeout(callback, 1000 / 60);
-          }
-      );
+    window.requestAnimationFrame =
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function(callback) {
+        return window.setTimeout(callback, 1000 / 60)
+      }
   }
-  const difference = Math.abs(from - to);
-  const step = Math.ceil(difference / duration * 50);
+  const difference = Math.abs(from - to)
+  const step = Math.ceil((difference / duration) * 50)
 
   function scroll(start, end, step) {
-      if (start === end) {
-          typeof callback === 'function' && callback();
-          return;
-      }
+    if (start === end) {
+      typeof callback === 'function' && callback()
+      return
+    }
 
-      let d = (start + step > end) ? end : start + step;
-      if (start > end) {
-          d = (start - step < end) ? end : start - step;
-      }
+    let d = start + step > end ? end : start + step
+    if (start > end) {
+      d = start - step < end ? end : start - step
+    }
 
-      if (el === window) {
-          window.scrollTo(d, d);
-      } else {
-          el.scrollTop = d;
-      }
-      window.requestAnimationFrame(() => scroll(d, end, step));
+    if (el === window) {
+      window.scrollTo(d, d)
+    } else {
+      el.scrollTop = d
+    }
+    window.requestAnimationFrame(() => scroll(d, end, step))
   }
 
-  scroll(from, to, step);
-};
+  scroll(from, to, step)
+}
--- validators.js
+++ validators.js
@@ -1,4 +1,4 @@
-export const isBrowser = typeof window !== "undefined";
+export const isBrowser = typeof window !== 'undefined'
 
 export const isMobile = value => {
   value = value.replace(/[^-|\d]/g, '')
@@ -7,14 +7,18 @@ export const isMobile = value => {
   )
 }
 
-export const isIOS = isBrowser ? !!(navigator && navigator.userAgent || '').match(/\(i[^;]+;( U;)? CPU.+Mac OS X/) : null;
+export const isIOS = isBrowser
+  ? !!((navigator && navigator.userAgent) || '').match(
+      /\(i[^;]+;( U;)? CPU.+Mac OS X/
+    )
+  : null
 
 export function isAndroid() {
-  return isBrowser ? /android/.test(navigator.userAgent.toLowerCase()) : null;
+  return isBrowser ? /android/.test(navigator.userAgent.toLowerCase()) : null
 }
 
 export const isObj = x => {
-  var type = typeof x
+  const type = typeof x
   return x !== null && (type === 'object' || type === 'function')
 }
 
@@ -26,32 +30,32 @@ export const range = (num, min, max) => {
   return Math.min(Math.max(num, min), max)
 }
 
-export const isColor = function (value) {
-    const colorReg = /^#([a-fA-F0-9]){3}(([a-fA-F0-9]){3})?$/;
-    const rgbaReg = /^[rR][gG][bB][aA]\(\s*((25[0-5]|2[0-4]\d|1?\d{1,2})\s*,\s*){3}\s*(\.|\d+\.)?\d+\s*\)$/;
-    const rgbReg = /^[rR][gG][bB]\(\s*((25[0-5]|2[0-4]\d|1?\d{1,2})\s*,\s*){2}(25[0-5]|2[0-4]\d|1?\d{1,2})\s*\)$/;
+export const isColor = function(value) {
+  const colorReg = /^#([a-fA-F0-9]){3}(([a-fA-F0-9]){3})?$/
+  const rgbaReg = /^[rR][gG][bB][aA]\(\s*((25[0-5]|2[0-4]\d|1?\d{1,2})\s*,\s*){3}\s*(\.|\d+\.)?\d+\s*\)$/
+  const rgbReg = /^[rR][gG][bB]\(\s*((25[0-5]|2[0-4]\d|1?\d{1,2})\s*,\s*){2}(25[0-5]|2[0-4]\d|1?\d{1,2})\s*\)$/
 
-    return colorReg.test(value) || rgbaReg.test(value) || rgbReg.test(value);
-};
+  return colorReg.test(value) || rgbaReg.test(value) || rgbReg.test(value)
+}
 
-export const hasClass = function (el, cls) {
-  cls = cls || '';
-  if (cls.replace(/\s/g, '').length === 0 || !el) return false;
-  return new RegExp(' ' + cls + ' ').test(' ' + el.className + ' ');
-};
+export const hasClass = function(el, cls) {
+  cls = cls || ''
+  if (cls.replace(/\s/g, '').length === 0 || !el) return false
+  return new RegExp(' ' + cls + ' ').test(' ' + el.className + ' ')
+}
 
-export const addClass = function (el, cls) {
+export const addClass = function(el, cls) {
   if (!hasClass(el, cls)) {
-      el.className = el.className === '' ? cls : el.className + ' ' + cls;
+    el.className = el.className === '' ? cls : el.className + ' ' + cls
   }
-};
+}
 
-export const removeClass = function (el, cls) {
+export const removeClass = function(el, cls) {
   if (hasClass(el, cls)) {
-      let newClass = ' ' + el.className.replace(/[\t\r\n]/g, '') + ' ';
-      while (newClass.indexOf(' ' + cls + ' ') >= 0) {
-          newClass = newClass.replace(' ' + cls + ' ', ' ');
-      }
-      el.className = newClass.replace(/^\s+|\s+$/g, '');
+    let newClass = ' ' + el.className.replace(/[\t\r\n]/g, '') + ' '
+    while (newClass.indexOf(' ' + cls + ' ') >= 0) {
+      newClass = newClass.replace(' ' + cls + ' ', ' ')
+    }
+    el.className = newClass.replace(/^\s+|\s+$/g, '')
   }
-};
+}
